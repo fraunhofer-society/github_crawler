bd75ba599203855a8ef4f28b6bbc0e9756b2779b
stefaneidelloth <eis@isi.fhg.de>
2024-01-11 11:31:46 +0100

    removed unused entries #1

--- renovate-config.js
+++ renovate-config.js
@@ -2,7 +2,7 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-// This is the configuration ofr renovate but, referenced from
+// This is the configuration of renovate bot, referenced from
 // github workflow .github/workflows/renovate.yml
 // Also see
 // https://github.com/renovatebot/github-action
@@ -34,22 +34,11 @@ module.exports = {
      "dependencyDashboardApproval": false,
      "minimumReleaseAge": null
     },
-
-    {
-      // sphinx-rtd-theme does not support Sphinx 7.0.1 yet  https://github.com/readthedocs/sphinx_rtd_theme/issues/1463
-      "matchPackageNames": ["Sphinx"],
-      "allowedVersions": "<7.0"
-    },
     {
       // licensecheck > 2023.1.4 has bugs regarding ignoreLicense option:
       // https://github.com/FHPythonUtils/LicenseCheck/issues/48
       "matchPackageNames": ["licensecheck"],
       "allowedVersions": "<=2023.1.4"
     },
-    {
-      // eslint-recommended/eslint-config@16.0.3 requires eslint-plugin-unicorn@"48.0.0"
-      "matchPackageNames": ["eslint-plugin-unicorn"],
-      "allowedVersions": "48.0.0"
-    }
   ]
 };
